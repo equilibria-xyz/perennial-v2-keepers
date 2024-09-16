e5041337cea82d8dbbfb7a8e367ad4692e2e2376
Arjun Rao <2940142+arjun-io@users.noreply.github.com>
2024-09-16 16:05:09 -0400

    Fix script

--- src/scripts/lensAbiCopy.ts
+++ src/scripts/lensAbiCopy.ts
@@ -2,7 +2,7 @@
 import fs from 'fs'
 import path from 'path'
 
-const lensJsonPath = path.join(__dirname, '../../artifacts/src/contracts/contracts/BatchKeeper.sol/BatchKeeper.json')
+const lensJsonPath = path.join(__dirname, '../../artifacts/src/contracts/BatchKeeper.sol/BatchKeeper.json')
 const lensJson = JSON.parse(fs.readFileSync(lensJsonPath, 'utf-8'))
 const tsCode = `export const BatchKeeperAbi = ${JSON.stringify(lensJson.abi)} as const;`
 const tsFilePath = path.join(__dirname, '../constants/abi/BatchKeeper.abi.ts')
